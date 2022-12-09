fbe4ce662102e4fd85d1da51c1608f402546a3e6
sunjinkang <2060037942@qq.com>
2022-12-09 18:29:27 +0800

    change file place

--- code_example/compress-picture/toBlob.js
+++ code_example/compress-picture/toBlob.js
@@ -3,12 +3,14 @@ function compressPicture(imgFile, imgType, quality) {
     imgType = 'image/webp';
   }
   return new Promise((resolve, reject) => {
+    // 返回一个URL 对象表示指定的 File 对象（生成blob:http://www.xxxx.com/xx的链接，可以直接在网页上打开File内容）或 Blob 对象（用于下载）
     let src = URL.createObjectURL(imgFile);
     let img = new Image();
     img.src = src;
     img.onload = function () {
       try {
         orginImg.src = src;
+        // 在每次调用 createObjectURL() 方法时，都会创建一个新的 URL 对象，即使你已经用相同的对象作为参数创建过。当不再需要这些 URL 对象时，每个对象必须通过调用 URL.revokeObjectURL() 方法来释放
         URL.revokeObjectURL(src);
         let width = Math.floor(this.width / 2);
         let height = Math.floor(this.height / 2);
